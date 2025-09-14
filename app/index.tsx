04422e3f475f867152017e5036853d3693577d04
rishabh21g <biasedengineer@gmail.com>
2025-09-14 21:17:02 +0530

    Route added

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,13 @@
-import { Text, View } from "react-native";
+import { Text } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function Index() {
+const FlappyBird = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView>
+      <Text>FlappyBird</Text>
+    </SafeAreaView>
   );
-}
+};
+
+export default FlappyBird;
