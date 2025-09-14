3e727ed0a86dbf15230275c8f23779a91805e665
rishabh21g <biasedengineer@gmail.com>
2025-09-14 23:52:12 +0530

    basic bg and base done

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,18 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "react-native";
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" />
-    </Stack>
+    <>
+      <StatusBar
+        backgroundColor={"black"}
+        translucent={false}
+        barStyle={"light-content"}
+        animated={true}
+      />
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
+      </Stack>
+    </>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,42 @@
-import { Text } from "react-native";
+import { Canvas, Image, useImage } from "@shopify/react-native-skia";
 import React from "react";
+import { useWindowDimensions } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const FlappyBird = () => {
-  return (
-    <SafeAreaView>
-      <Text>FlappyBird</Text>
-    </SafeAreaView>
-  );
-};
+  const width = useWindowDimensions().width;
+  const height = useWindowDimensions().height;
+  const bgImage = useImage(require("../assets/sprites/background-day.png"));
+  const baseImage = useImage(require("../assets/sprites/base.png"));
+  const baseHeight = 140; 
 
+  return (
+    <>
+      <SafeAreaView style={{ flex: 1 }}>
+        <Canvas style={{ width, height }}>
+          {bgImage && (
+            <Image
+              image={bgImage}
+              x={0}
+              y={0}
+              width={width}
+              height={height}
+              fit={"cover"}
+            />
+          )}
+          {baseImage && (
+            <Image
+              image={baseImage}
+              x={0}
+              y={height - baseHeight}
+              width={width}
+              height={baseHeight}
+              fit={"cover"}
+            />
+          )}
+        </Canvas>
+      </SafeAreaView>
+    </>
+  );
+};;
 export default FlappyBird;
